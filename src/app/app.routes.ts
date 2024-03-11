983b30c94e6ac14f88400654764382feaf9ac7c9
aryan9867bar <aryan123web@gmail.com>
2024-03-11 19:54:23 +0545

    search-component: Addded HTML code for search box

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "dashboard",
+        component: DashboardComponent,
+    },
+
+    {
+        path: "movie-list",
+        component: MovieListComponent,
+    },
+
+    {
+        path: "movie-details",
+        component: MovieDetailsComponent,
+    },
+    {
+        path:"",
+        redirectTo:"/dashboard",
+        pathMatch:"full"
+      },
+];
